c7c0204399e7b5460644d76ed58d4fa2be79a677
aakankshadub <73836342+aakankshadub@users.noreply.github.com>
2020-11-02 20:38:04 +0530

    Add files via upload

---
+++ form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  first='';
+  last='';
+  address='';
+  email='';
+  pass='';
+  ndate='';
+}
